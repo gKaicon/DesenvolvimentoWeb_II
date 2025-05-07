2469d6e8489e32076e433bddf1badd37b6c6efd0
GABRIEL KAICON BATISTA HILARIO <0048609@IFMG.EDU.BR>
2025-05-07 11:13:52 -0300

    Att Lab 3 07/05

---
+++ Atividade 3/resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<style>
+	footer {
+        display: flex;
+        justify-content: center;
+		width: 100%;
+		height: 30px;
+		position: fixed;
+		bottom: 0;
+	}
+</style>
+<footer class="bg-dark text-white">
+	<p>&copy; Desenvolvimento Web II 2025.</p>
+</footer>
+
