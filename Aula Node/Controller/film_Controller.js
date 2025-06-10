c924bbb9d10bfc1febd2995ef4188b8e6c54605b
Gabriel Káicon <gkaicon2@gmail.com>
2025-06-10 00:20:13 -0300

    Adição da prototipagem

--- Aula Node/Controller/film_Controller.js
+++ Aula Node/Controller/film_Controller.js
@@ -1,11 +1,12 @@
 import Film from "../Models/film.js";
 import Gender from "../Models/gender.js";
+import Actor from "../Models/actor.js";
 
 async function createFilm(req, res) {
     const actors = [];
 
     for (let i = 0; i < req.body.actors.length; i++) {
-        const actor= await Actor.findByPk(req.body.actors[i]);
+        const actor = await Actor.findByPk(req.body.actors[i]);
         actors.push(actor);
     }
 
@@ -15,25 +16,26 @@ async function createFilm(req, res) {
         release_year: req.body.release_year,
         genderId: req.body.genderId
     });
+    await newFilm.addActors(actors);
     res.status(201).json(newFilm);
 }
 
 async function listFilms(req, res) {
-
-    const films = await Film.findAll({include: [Gender, Actor], raw: true});
+    const films = await Film.findAll({ include: [{ model: Gender, as: 'gender' }, Actor] });
     res.json(films);
 }
 
-async function updateFilm(req, res){
-    const { title, description, release_year, id }  = req.body
-    const updateFilm = await Film.update({ title, description, release_year } , {where: {id: id}})
+async function updateFilm(req, res) {
+
+    const { title, description, release_year, id, genderId } = req.body
+    const updateFilm = await Film.update({ title, description, release_year, genderId }, { where: { id: id } })
     res.status(200).json(updateFilm)
 }
 
-async function deleteFilm(req, res){
+async function deleteFilm(req, res) {
     const id = req.body.id
-    await Film.destroy({where: {id: id}})
-    res.status(200).json({message : "Registro: " + id + " removido"})
+    await Film.destroy({ where: { id: id } })
+    res.status(200).json({ message: "Registro: " + id + " removido" })
 }
 
 export { createFilm, listFilms, updateFilm, deleteFilm };
--- Aula Node/database/0-QuerySQL.sql
+++ Aula Node/database/0-QuerySQL.sql
@@ -10,8 +10,8 @@ insert into genders values  ('1', 'Ação', '2025-06-06 03:06:42', '2025-06-06 0
                             ('4', 'Romance', '2025-06-06 03:06:42', '2025-06-06 03:06:42'),
                             ('5', 'Aventura', now(), now());
 
-insert into actors values   ('1', 'Alice', 'Smith', '1990-05-15 00:00:00', '555-123-4567', 'alice.smith@example.com', '2025-06-01 20:31:55', '2025-06-01 20:31:55'),
-                            ('2', 'Bob', 'Singer', '1960-05-15 00:00:00', '545-953-4567', 'singer.bob@example.com', '2025-06-01 20:33:33', '2025-06-01 20:33:33');
+insert into actors values   (null, 'Alice', 'Smith', '1990-05-15 00:00:00', '555-123-4567', 'alice.smith@example.com', '2025-06-01 20:31:55', '2025-06-01 20:31:55'),
+                            (null, 'Bob', 'Singer', '1960-05-15 00:00:00', '545-953-4567', 'singer.bob@example.com', '2025-06-01 20:33:33', '2025-06-01 20:33:33');
 
 insert into films values    ('1', 'Sonic 3 - Shadow ', 'Terceiro filme da franquia Live Action de Sonic the Hedgehog', '2025', '2025-06-01 20:11:12', '2025-06-01 20:20:32', 5),
                             ('2', 'Scary Movie 2', 'Segundo filme da franquia da paródia de humor de filmes de Terror', '2001', '2025-06-01 20:22:51', '2025-06-01 20:22:51', 2);
