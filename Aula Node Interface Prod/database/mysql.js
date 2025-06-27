cd087bcc9175fb4d246c437324aa45ce7c7e77e9
Gabriel Káicon <gkaicon2@gmail.com>
2025-06-27 19:09:28 -0300

    Att Casa 27/06

--- Aula Node Interface Prod/database/mysql.js
+++ Aula Node Interface Prod/database/mysql.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'atividade_node'
-});
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: 'root',
+//     database: 'atividade_node'
+// });
+
+const sequelize = new Sequelize('postgresql://pratica_filmes_user:AlRBW7UiuLNaSxxhv9YtCTFFeEHrmdoM@dpg-d1fhbn2dbo4c73fjc9ag-a/pratica_filmes_db_tfu1');
 
 export default sequelize;
